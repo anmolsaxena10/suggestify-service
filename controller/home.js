690763bf107d4486f21d6d20f072e3692d8db717
anmolsaxena10 <anmol.saxena10@gmail.com>
2019-03-19 17:12:15 +0530

    home updated

--- controller/home.js
+++ controller/home.js
@@ -1,6 +1,7 @@
 module.exports = function(req, res){
+    var demoToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5hbnRfaWQiOiI1YzdiOTQ4M2RmYjczZjVjNGY0NTIwNGYiLCJlbWFpbCI6ImFubW9sLnNheGVuYTEwQGdtYWlsLmNvbSIsImFwcF9uYW1lIjoiQW5tb2wgU2F4ZW5hIiwiaWF0IjoxNTUyOTkyNTMwfQ.97eGLJSOloqgBFSg7fdJmVXBkKELGWz0VlmKAVIJpAI';
     res.render('home.html', {
         'menu': req.menu,
-        'token': req.session.jwtToken
+        'token': demoToken
     });
 }
--- public/js/script.js
+++ public/js/script.js
@@ -2,3 +2,8 @@ document.addEventListener('DOMContentLoaded', function () {
     var elems = document.querySelectorAll('.collapsible');
     var instances = M.Collapsible.init(elems);
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems);
+  });
